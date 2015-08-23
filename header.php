034b0ef6db4dd171e41337a21aa7e8b599c2d610
k3jo <joanne.kwh@gmail.com>
2015-08-23 13:47:56 -0400

    updated styles

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
   <!-- FONT AWESOME -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <!-- HOVER EFFECT -->
-  <link rel="stylesheet" href="animate.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.3.0/animate.css">
   <!-- STYLE -->
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
